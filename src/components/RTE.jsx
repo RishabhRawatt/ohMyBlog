a01b68ddb027f018440db027a54b2187a83b5ddf
Rishabh Rawat <rishabh2001rawat@gmail.com>
2024-02-03 16:29:03 +0530

    post form

---
+++ src/components/RTE.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+// this used to control inputs of external components or custom components
+import { Controller } from "react-hook-from";
+
+// this control will pass on the control whoever call this RTE
+export default function RTE({ name, control, label, defaultValue = "" }) {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+
+      {/* these are parameters passed to whoever called this component */}
+      <Controller
+        name={name || "content"}
+        control={control}
+        // in render is that tells whenever onchange occure notify me
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
+}
