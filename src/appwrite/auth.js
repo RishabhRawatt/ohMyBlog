d3d3b940df0b33a04dbe4f57b8ae0fc67c633754
Rishabh Rawat <rishabh2001rawat@gmail.com>
2024-02-24 10:55:48 +0530

    error message

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -40,6 +40,7 @@ export class AuthService {
       } else return userAccount;
     } catch (error) {
       console.log("createAcount :: error", error);
+      throw error;
     }
   }
 
@@ -50,6 +51,7 @@ export class AuthService {
       return await this.account.createEmailSession(email, password);
     } catch (error) {
       console.log("login :: error", error);
+      throw error;
     }
   }
 
